2d93852ef35dbd874368884a8435ee018fd57671
jrlepere <jrlepere1@gmail.com>
2018-02-05 16:12:37 -0800

    Simmulated Annealing

--- search_library/src/local_search/SimulatedAnnealing.java
+++ search_library/src/local_search/SimulatedAnnealing.java
@@ -1,6 +1,12 @@
 package local_search;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import problem.Action;
 import problem.Problem;
+import problem.State;
 import search.IResultObject;
 import search.LocalSearch;
 
@@ -11,15 +17,39 @@ import search.LocalSearch;
  */
 public class SimulatedAnnealing extends LocalSearch {
 
-	public SimulatedAnnealing(ObjectiveFunction fun) {
+	public SimulatedAnnealing(ObjectiveFunction fun, Scheduler scheduler) {
 		super(fun);
-		// TODO Auto-generated constructor stub
+		this.scheduler = scheduler;
 	}
-
-	@Override
+	
 	public IResultObject execute(Problem p) {
-		// TODO Auto-generated method stub
-		return null;
+		State currentState = p.getInitialState();
+		while (true) {
+			double T = this.scheduler.getNextT();
+			if (T == 0) return new LocalSearchResult(currentState);
+			List<State> successors = new ArrayList<>();
+			for (Action a : p.getActions()) {
+				if (!a.canExecute(currentState)) continue;
+				successors.add(a.execute(currentState));
+			}
+			if (successors.isEmpty()) return new LocalSearchResult(currentState);
+			State successorState = successors.get(gen.nextInt(successors.size()));
+			double delta = this.function.execute(successorState) - this.function.execute(currentState);
+			if (delta > 0) currentState = successorState;
+			else {
+				if (gen.nextDouble() < Math.exp(delta/T)) currentState = successorState;
+			}
+		}
+		
 	}
+	
+	public interface Scheduler {
+		
+		public double getNextT();
+		
+	}
+	
+	private Scheduler scheduler;
+	Random gen = new Random();
 
 }
